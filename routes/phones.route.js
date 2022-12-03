6c1b68444e943883c20793919e7544029554d1a6
zelkarnayn <zelkarnayn@gmail.com>
2022-12-04 02:14:30 +0300

    Сделан сервер с добавлением и выведением телефонов

---
+++ routes/phones.route.js
@@ -0,0 +1,8 @@
+const { Router } = require('express')
+const { phonesController } = require('../controllers/phones.controller')
+const router = Router()
+
+router.get('/phones/', phonesController.getPhones)
+router.post('/phones/', phonesController.addPhone)
+
+module.exports = router
